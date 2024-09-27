ce21aa3e189f9bf7e9752b26d704ef35cc41b643
AGonzL <agonzlopez.11@gmail.com>
2024-09-27 15:56:46 +0200

    Ejercicio 11 completado

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -4,8 +4,36 @@ public class Exercise11 {
     public static void main(String[] args) {
         TVRemote redRemote = new TVRemote("Rojo");
         TVRemote blackRemote = new TVRemote("Negro");
-        System.out.println(redRemote.getColor());
-        System.out.println(blackRemote.getColor());
-    }
 
+        // Encender la TV con el mando rojo
+        redRemote.turnOn();
+
+        // Subir y bajar el canal
+        redRemote.channelUp();
+        System.out.println("Canal actual (después de subir): " + redRemote.getChannel());
+
+        redRemote.channelDown();
+        System.out.println("Canal actual (después de bajar): " + redRemote.getChannel());
+
+        // Intentar bajar el canal cuando ya está en 0
+        redRemote.channelDown();
+        System.out.println("Canal actual (después de intentar bajar en 0): " + redRemote.getChannel());
+
+        // Subir y bajar el volumen
+        redRemote.volumeUp();
+        System.out.println("Volumen actual (después de subir): " + redRemote.getVolume());
+
+        redRemote.volumeDown();
+        System.out.println("Volumen actual (después de bajar): " + redRemote.getVolume());
+
+        // Intentar bajar el volumen cuando ya está en 0
+        redRemote.volumeDown();
+        System.out.println("Volumen actual (después de intentar bajar en 0): " + redRemote.getVolume());
+
+        // Obtener y mostrar el color del mando
+        System.out.println("El color del mando es: " + redRemote.getColor());
+
+        // Apagar la TV
+        redRemote.turnOff();
+    }
 }
--- src/main/java/com/campusdual/classroom/TVRemote.java
+++ src/main/java/com/campusdual/classroom/TVRemote.java
@@ -2,4 +2,60 @@ package com.campusdual.classroom;
 
 public class TVRemote {
 
+    public int channel;
+    public int volume;
+    public boolean on;
+    private final String color;
+
+    public TVRemote(String color) {
+        this.color = color;
+        this.channel = 0;
+        this.volume = 0;
+        this.on = false;
+    }
+
+    public void turnOn() {
+        this.on = true;
+    }
+
+    public void turnOff() {
+        this.on = false;
+    }
+
+    public void channelUp() {
+        this.channel++;
+    }
+
+    public void channelDown() {
+        if (this.channel > 0) {
+            this.channel--;
+        }
+    }
+
+    public void volumeUp() {
+        this.volume++;
+    }
+
+    public void volumeDown() {
+        if (this.volume > 0) {
+            this.volume--;
+        }
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+
+    public int getChannel() {
+        return channel;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
 }
